9f1ae51f1184754268eb6cb0fa781689eabde836
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-18 07:25:00 +0530

    Pushed: basic files

---
+++ adminUI/src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+    // https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json
+  return (
+    <>
+      <div>Admin Dashboard</div>;
+
+    </>
+  );
+};
+
+export { Dashboard };
