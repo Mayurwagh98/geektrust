eea8365736671722374d9c4ed9173b4fcf56c474
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-23 11:11:51 +0530

    Added: search by name or type or color

--- TeeRex_Store/src/components/Home.jsx
+++ TeeRex_Store/src/components/Home.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Home.css";
+import { Filter } from "./Filter";
 
 const Home = () => {
   let [products, setProducts] = useState([]);
 
+  let [search, setSearch] = useState("");
+
   let getData = async () => {
     await axios
-      .get("http://localhost:8080/products")
+      .get(`http://localhost:8080/products?q=${search}`)
       .then((res) => {
         console.log(res.data);
         setProducts(res.data);
@@ -17,21 +20,37 @@ const Home = () => {
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [search]);
 
   return (
-    <div className="main_products_div">
-      {products?.map((item, index) => {
-        return (
-          <div key={index}>
-            <img src={item.imageURL} alt="image" />
-            <h2>{item.name}</h2>
-            <p>{item.price}</p>
-            <button>Add to Cart</button>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div>
+        {/* ------------ filter --------------- */}
+        <Filter setProducts={setProducts} />
+
+        {/*---------------- search -------------- */}
+        <input
+          type="text"
+          placeholder="Search by name or type or color"
+          className="search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        {/*-------------------- data mapped --------------- */}
+        <div className="main_products_div">
+          {products?.map((item, index) => {
+            return (
+              <div key={index}>
+                <img src={item.imageURL} alt="image" />
+                <h2>{item.name}</h2>
+                <p>{item.price}</p>
+                <button>Add to Cart</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
