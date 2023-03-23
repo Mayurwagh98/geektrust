60d7fa33a8563a66e9af4435d2c0e42755f06bd4
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-23 10:09:10 +0530

    Allowing: User to select only 1 filter

--- TeeRex_Store/src/components/Filter.jsx
+++ TeeRex_Store/src/components/Filter.jsx
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const Filter = ({ setProducts }) => {
+  let [selected, setSelected] = useState(null);
+
   let handleCheckbox = async (event) => {
     // console.log(event.target.value);
+    setSelected(event.target.value);
     await axios
       .get(`http://localhost:8080/products?color=${event.target.value}`)
       .then((res) => {
@@ -16,15 +19,40 @@ const Filter = ({ setProducts }) => {
   return (
     <div>
       <h1>Filter</h1>
-      <input type="checkbox" value="Black" onChange={handleCheckbox} />
+      <input
+        type="checkbox"
+        value="Black"
+        checked={selected === "Black"} // allowing user to select only one checkbox
+        onChange={handleCheckbox}
+      />
       <label>Black</label>
-      <input type="checkbox" value="Pink" onChange={handleCheckbox} />
+      <input
+        type="checkbox"
+        value="Pink"
+        checked={selected === "Pink"} // allowing user to select only one checkbox
+        onChange={handleCheckbox}
+      />
       <label>Pink</label>
-      <input type="checkbox" value="Green" onChange={handleCheckbox} />
+      <input
+        type="checkbox"
+        value="Green"
+        checked={selected === "Green"} // allowing user to select only one checkbox
+        onChange={handleCheckbox}
+      />
       <label>Green</label>
-      <input type="checkbox" value="Blue" onChange={handleCheckbox} />
+      <input
+        type="checkbox"
+        value="Blue"
+        checked={selected === "Blue"} // allowing user to select only one checkbox
+        onChange={handleCheckbox}
+      />
       <label>Black</label>
-      <input type="checkbox" value="Red" onChange={handleCheckbox} />
+      <input
+        type="checkbox"
+        value="Red"
+        checked={selected === "Red"} // allowing user to select only one checkbox
+        onChange={handleCheckbox}
+      />
       <label>Red</label>
     </div>
   );
