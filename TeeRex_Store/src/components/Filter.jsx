6b965f18b03bfe90afa4faf71b25efca5a19aecf
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-28 17:43:43 +0530

    Added: searcParams

--- TeeRex_Store/src/components/Filter.jsx
+++ TeeRex_Store/src/components/Filter.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useSearchParams } from "react-router-dom";
 
 const Filter = ({ setProducts }) => {
   let [selected, setSelected] = useState(null);
+  let [searchParams, setSearchParams] = useSearchParams();
+  let [category, setCategory] = useState(searchParams.getAll("color") || []);
 
   let handleCheckbox = async (event) => {
     // console.log(event.target.value);
@@ -33,6 +36,30 @@ const Filter = ({ setProducts }) => {
     }
   };
 
+  let handleFilter = (event) => {
+    let option = event.target.value;
+    //logic --> if the option is present in the category array, remove it,
+    //else add it to the category array
+
+    let newCategory = [...category]; //making the copy of the category
+    if (newCategory.includes(option)) {
+      //remove it
+      newCategory.splice(newCategory.indexOf(option), 1);
+    } else {
+      // add it
+      newCategory.push(option);
+    }
+    setCategory(newCategory);
+  };
+  console.log(category);
+
+  useEffect(() => {
+    //used useEffect because every time user clicks on filter, search param should change and DOM should re-render
+    let params = {};
+    category && (params.color = category);
+    setSearchParams(params);
+  }, [category, setSearchParams]);
+
   return (
     <div>
       <section>
@@ -40,30 +67,34 @@ const Filter = ({ setProducts }) => {
         <input
           type="checkbox"
           value="Black"
-          checked={selected === "Black"} // allowing user to select only one checkbox
-          onChange={handleCheckbox}
+          // checked={selected === "Black"}
+          defaultChecked={category.includes("Black")} // allowing user to select only one checkbox
+          onChange={handleFilter}
         />
         <label>Black</label>
         <input
           type="checkbox"
           value="Pink"
-          checked={selected === "Pink"} // allowing user to select only one checkbox
-          onChange={handleCheckbox}
+          // checked={selected === "Pink"} // allowing user to select only one checkbox
+          defaultChecked={category.includes("Pink")}
+          onChange={handleFilter}
         />
         <label>Pink</label>
         <input
           type="checkbox"
           value="Green"
-          checked={selected === "Green"} // allowing user to select only one checkbox
-          onChange={handleCheckbox}
+          // checked={selected === "Green"} // allowing user to select only one checkbox
+          defaultChecked={category.includes("Green")}
+          onChange={handleFilter}
         />
         <label>Green</label>
 
         <input
           type="checkbox"
           value="Red"
-          checked={selected === "Red"} // allowing user to select only one checkbox
-          onChange={handleCheckbox}
+          // checked={selected === "Red"} // allowing user to select only one checkbox
+          defaultChecked={category.includes("Red")}
+          onChange={handleFilter}
         />
         <label>Red</label>
       </section>
@@ -73,14 +104,14 @@ const Filter = ({ setProducts }) => {
           type="checkbox"
           value="Men"
           checked={selected === "Men"} // allowing user to select only one checkbox
-          onChange={handleCheckbox}
+          onChange={handleFilter}
         />
         <label>Men</label>
         <input
           type="checkbox"
           value="Women"
           checked={selected === "Women"} // allowing user to select only one checkbox
-          onChange={handleCheckbox}
+          onChange={handleFilter}
         />
         <label>Women</label>
       </section>
