5b63b2a16b41d924143d8d9fcf69734b6bd27f8c
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-28 17:43:15 +0530

    Retaining: Retaining the filter event if component is re-render

--- TeeRex_Store/src/components/Home.jsx
+++ TeeRex_Store/src/components/Home.jsx
@@ -2,15 +2,19 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Home.css";
 import { Filter } from "./Filter";
+import { Button } from "antd";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 const Home = () => {
   let [products, setProducts] = useState([]);
 
   let [search, setSearch] = useState("");
+  let [searchParams] = useSearchParams();
+  let location = useLocation();
 
-  let getData = async () => {
+  let getData = async (queryParams) => {
     await axios
-      .get(`http://localhost:8080/products?q=${search}`)
+      .get(`http://localhost:8080/products?q=${search}`, queryParams)
       .then((res) => {
         console.log(res.data);
         setProducts(res.data);
@@ -19,8 +23,30 @@ const Home = () => {
   };
 
   useEffect(() => {
-    getData();
-  }, [search]);
+    if (location || products.length === 0) {
+      let color = searchParams.getAll("color");
+      let queryParams = {
+        params: {
+          color: color,
+        },
+      };
+      getData(queryParams);
+    }
+  }, [search, location.search]);
+
+  // ----------- add to cart ---------------
+  let handleCart = (item) => {
+    let cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+    for (let x of cartItems) {
+      if (x.id == item.id) {
+        return alert("Item already in the cart");
+      }
+    }
+    let qty = 1;
+    item = { ...item, qty };
+    cartItems.push(item);
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  };
 
   return (
     <>
@@ -43,8 +69,10 @@ const Home = () => {
               <div key={index}>
                 <img src={item.imageURL} alt="image" />
                 <h2>{item.name}</h2>
-                <p>{item.price}</p>
-                <button>Add to Cart</button>
+                <p>₹{item.price}</p>
+                <Button type="primary" onClick={() => handleCart(item)}>
+                  Add to Cart
+                </Button>
               </div>
             );
           })}
