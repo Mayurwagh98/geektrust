5351db1b54cdcf4c56c1fecdc14bb423c69ae88b
Mayurwagh98 <mswagh1998@gmail.com>
2023-04-03 18:16:56 +0530

    Importing: Products from redux store

--- TeeRex_Store/src/components/Home.jsx
+++ TeeRex_Store/src/components/Home.jsx
@@ -1,26 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { getProducts } from "../redux/action";
 import "./Home.css";
 import { Filter } from "./Filter";
 import { Button } from "antd";
 import { useLocation, useSearchParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
-  let [products, setProducts] = useState([]);
-
+  // let [products, setProducts] = useState([]);
+  let products = useSelector((store) => store.products);
+  console.log(products);
   let [search, setSearch] = useState("");
   let [searchParams] = useSearchParams();
   let location = useLocation();
-
-  let getData = async (queryParams) => {
-    await axios
-      .get(`http://localhost:8080/products?q=${search}`, queryParams)
-      .then((res) => {
-        console.log(res.data);
-        setProducts(res.data);
-      })
-      .catch((e) => console.log(e));
-  };
+  let dispatch = useDispatch();
 
   useEffect(() => {
     if (location || products.length === 0) {
@@ -30,7 +24,7 @@ const Home = () => {
           color: color,
         },
       };
-      getData(queryParams);
+      dispatch(getProducts(queryParams, search));
     }
   }, [search, location.search]);
 
@@ -52,7 +46,7 @@ const Home = () => {
     <>
       <div>
         {/* ------------ filter --------------- */}
-        <Filter setProducts={setProducts} />
+        <Filter />
 
         {/*---------------- search -------------- */}
         <input
@@ -64,7 +58,7 @@ const Home = () => {
 
         {/*-------------------- data mapped --------------- */}
         <div className="main_products_div">
-          {products?.map((item, index) => {
+          {products.map((item, index) => {
             return (
               <div key={index}>
                 <img src={item.imageURL} alt="image" />
